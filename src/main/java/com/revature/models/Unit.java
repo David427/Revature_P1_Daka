c6648724882052f2bbab42013955a0f4fe569b80
David427 <david427@live.com>
2022-01-29 09:06:04 -0500

    Started thinking about how to generate a list of all classes with @Entity

--- src/main/java/com/revature/models/Unit.java
+++ src/main/java/com/revature/models/Unit.java
@@ -2,7 +2,18 @@ package com.revature.models;
 
 import com.revature.annotations.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Unit {
+
+    //region USING REFLECTION TO GET ALL CLASSES WITH @ENTITY
+    //May need https://mvnrepository.com/artifact/org.reflections/reflections/0.10.2
+    //David sent an email asking Ryan if our library, which is meant to be used as a dependency for the web app,
+    //can have dependencies of its own. Will Maven load both Daka and the Reflections dependency that it needs
+    //to work? Probably... but I want to make sure.
+    //endregion
+
     //region USING REFLECTION TO READ ANNOTATION VALUES
     Class<Unit> unit = Unit.class;
     Entity annotation = unit.getAnnotation(Entity.class);
--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -1,2 +1,27 @@
-public class RunnerTest {
+import com.revature.annotations.Column;
+import com.revature.annotations.Entity;
+import com.revature.annotations.Table;
+import com.revature.models.Unit;
+import org.junit.jupiter.api.Test;
+
+@Entity
+@Table(name = "test_1")
+public class RunnerTest extends Unit {
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "type")
+    private String type;
+    @Column(name = "available")
+    private boolean available;
+
+    @Test
+    public static void main(String[] args) {
+        Class<RunnerTest> rt = RunnerTest.class;
+        Table table = rt.getAnnotation(Table.class);
+
+        System.out.println(table.name());
+
+    }
 }
